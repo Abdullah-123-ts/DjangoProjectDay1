8ecdb62478bab8ec4f4cb8f441de09739a48469e
AbdullahGhulamNabi <abdullah.ghulamnabi@techtics.ai>
2025-03-19 19:55:16 +0500

    task2 part1 done

--- myapp/ClassBasedViewsBook/api/urls.py
+++ myapp/ClassBasedViewsBook/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from ClassBasedViewsBook.api.views import ShowAllBooks
+from ClassBasedViewsBook.api.views import ShowAllBooks, RetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path("AddShowBooks/", ShowAllBooks.as_view() , name='all_books'),
+    path("AddShowBooks/", ShowAllBooks.as_view() , name='all_books'),
+    path("AddShowBooks/<int:id>", RetrieveUpdateDestroyAPIView.as_view() , name='get_individual_books'),
 ]
--- myapp/ClassBasedViewsBook/api/views.py
+++ myapp/ClassBasedViewsBook/api/views.py
@@ -1,7 +1,41 @@
 from book.models import Book
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 from book.serializers import BookSerializer
 
 class ShowAllBooks(ListCreateAPIView):
-        queryset = Book.objects.all()
-        serializer_class = BookSerializer
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class RetrieveUpdateDestroyAPIView(APIView):
+    
+    def get(self, request, id):
+        try:
+            queryset = Book.objects.get(id=id)
+            serialized_data = BookSerializer(queryset)
+            return Response(serialized_data.data, status=status.HTTP_200_OK)
+        except Book.DoesNotExist:
+            return Response({'error':"Book not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+    def put(self, request, id):
+        try:
+            queryset = Book.objects.get(id=id)
+            serialized_data = BookSerializer(queryset, data=request.data)
+            if(serialized_data.is_valid()):
+                serialized_data.save()
+                return Response(serialized_data.data, status=status.HTTP_200_OK)
+        except Book.DoesNotExist:
+            return Response({'error':"Book not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+
+
+    def delete(self, request, id):
+        try:
+            queryset = Book.objects.get(id=id)
+            queryset.delete()
+            return Response(status=status.HTTP_200_OK)
+        except Book.DoesNotExist:
+            return Response({'error':"Book not found"}, status=status.HTTP_404_NOT_FOUND)
