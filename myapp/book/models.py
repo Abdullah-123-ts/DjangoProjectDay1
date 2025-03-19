be6357a11d1be0dd77f0690f89be384f6a9b9074
AbdullahGhulamNabi <abdullah.ghulamnabi@techtics.ai>
2025-03-19 17:10:02 +0500

    Done with day1 tasks

--- myapp/book/models.py
+++ myapp/book/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title           = models.CharField(max_length=255, blank=True, unique=True)
+    author          = models.CharField(max_length=255, blank=True, unique=True)
+    Published_date  = models.DateTimeField(null=True, blank=True)
